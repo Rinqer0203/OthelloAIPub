80700d4558a8262b34a69a5b92ee2950b9c97fde
RinqerNote\Tomoki <furukawa12122003@gmail.com>
2023-11-30 12:39:26 +0900

    30ターンをすぎるとisActiveModeフラグ変数がTrueになる実装を追加。コメント追加

--- OthelloAction.py
+++ OthelloAction.py
@@ -2,10 +2,25 @@ import random
 import Evaluate
 from typing import List
 
+turnCnt = 0
+isActiveMode = False
+
 
 def getAction(board, moves) -> List[int]:
     # 自分のターンにフロントエンドから呼ばれるメソッド
+    check_active_mode()
+    # 現在の状態をデバッグ出力
+    # print(f"turnCnt: {turnCnt} isActiveMode: {isActiveMode}")
     return select_random_moves(moves)
+
+
+def check_active_mode():
+    global turnCnt
+    global isActiveMode
+
+    if turnCnt > 30:
+        isActiveMode = True
+    turnCnt += 2
 
 
 def select_random_moves(moves) -> List[int]:
