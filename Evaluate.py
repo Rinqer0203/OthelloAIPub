9df3d047ba87fc543f6f5f96f3a2f376f2667640
RinqerNote\Tomoki <furukawa12122003@gmail.com>
2023-11-30 13:29:35 +0900

    moveの評価ができるように変更。OthelloAction内でmoveの評価値が最も高い手を打つように変更。TestBoardのりファクタとパターンを増やした。

--- Evaluate.py
+++ Evaluate.py
@@ -1,7 +1,7 @@
 import OthelloLogic
 import TestBoardProvider
 
-# 評価関数 evaluate(self, board, player)を実装したクラス定義
+# Boardに対する評価関数 evaluate(self, board, player)を実装したクラス定義
 
 
 class __TestEvaluate:
@@ -22,21 +22,65 @@ class __TestEvaluate3:
         return 3
 
 
-# -----------------ここまで評価関数の定義-----------------
+# -----------------ここまでBoard評価関数の定義-----------------
 
+
+# Moveに対する評価関数 evaluate(self, board, move, player)を実装したクラス定義
+
+
+class __EvaluateBaseBoard:
+    baseBoardPoint = [
+        [30, -10, 2, 1, 1, 2, -10, 30],
+        [-10, -20, -3, -3, -3, -3, -20, -10],
+        [2, -3, 2, 0, 0, 2, -3, 2],
+        [1, -3, 0, 0, 0, 0, -3, 1],
+        [1, -3, 0, 0, 0, 0, -3, 1],
+        [2, -3, 2, 0, 0, 2, -3, 2],
+        [-10, -20, -3, -3, -3, -3, -20, -10],
+        [30, -10, 2, 1, 1, 2, -10, 30],
+    ]
+
+    def evaluate(self, board, move, player):
+        return self.baseBoardPoint[move[0]][move[1]]
+
+
+# -----------------ここまでMove評価関数の定義-----------------
 
 # 初期化処理
 # 評価関数を追加したらここに追加する
-__evalute_funcs = [__TestEvaluate(), __TestEvaluate2(), __TestEvaluate3()]
+__evalute_board_funcs = [__TestEvaluate(), __TestEvaluate2(), __TestEvaluate3()]
+
+__evaluate_move_funcs = [__EvaluateBaseBoard()]
 
 
-# 評価関数を回して評価値を返す
+# 評価関数を回して評価値の合計を返す
 def evaluate_board(board, player) -> float:
     evaluate_point = 0
-    for func in __evalute_funcs:
+    for func in __evalute_board_funcs:
         evaluate_point += func.evaluate(board, player)
     return evaluate_point
 
 
-print(evaluate_board(TestBoardProvider.get_initial_board(), 1))
-# 　testのみだと10が出力される
+def evaluate_move(board, move, player) -> float:
+    # 評価関数を回して評価値の合計を返す
+    evaluate_point = 0
+    for func in __evaluate_move_funcs:
+        evaluate_point += func.evaluate(board, move, player)
+    return evaluate_point
+
+
+# テスト用
+if __name__ == "__main__":
+    board = TestBoardProvider.generate_initial_board()
+
+    print("boardの評価値を返す関数のテスト")
+    eval = evaluate_board(board, 1)
+    print(eval)
+
+    print("moveの評価値を返す関数のテスト")
+    for x in range(8):
+        for y in range(8):
+            move = [x, y]
+            eval = evaluate_move(board, move, 1)
+            print(eval, end=" | ")
+        print()
--- OthelloAction.py
+++ OthelloAction.py
@@ -8,10 +8,11 @@ isActiveMode = False
 
 def getAction(board, moves) -> List[int]:
     # 自分のターンにフロントエンドから呼ばれるメソッド
+
     check_active_mode()
-    # 現在の状態をデバッグ出力
     # print(f"turnCnt: {turnCnt} isActiveMode: {isActiveMode}")
-    return select_random_moves(moves)
+
+    return select_max_eval_moves(board, moves, 1)
 
 
 def check_active_mode():
@@ -21,6 +22,18 @@ def check_active_mode():
     if turnCnt > 30:
         isActiveMode = True
     turnCnt += 2
+
+
+def select_max_eval_moves(board, moves, player) -> List[int]:
+    maxEvalMove = float("-inf"), None
+
+    for move in moves:
+        eval = Evaluate.evaluate_move(board, move, player)
+        if maxEvalMove[0] < eval:
+            maxEvalMove = eval, move
+
+    print(f"決定した手: {maxEvalMove[1]} 評価値: {maxEvalMove[0]}")
+    return maxEvalMove[1]
 
 
 def select_random_moves(moves) -> List[int]:
