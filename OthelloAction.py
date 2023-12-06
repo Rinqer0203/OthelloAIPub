44399c752ad2176466d492d63ac113bac6d3bd24
RinqerNote\Tomoki <furukawa12122003@gmail.com>
2023-12-06 13:30:08 +0900

    最弱モードの仮実装。テストケースの追加。

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,5 +1,7 @@
 import random
+import OthelloLogic
 import Evaluate
+import copy
 from typing import List
 
 turnCnt = 0
@@ -11,7 +13,18 @@ def getAction(board, moves) -> List[int]:
 
     check_active_mode()
 
-    return select_max_eval_moves(board, moves, 1)
+    # return select_max_eval_moves(board, moves, 1)
+    return inactive_action(board, moves, 1)
+
+
+def inactive_action(board, moves, player):
+    """
+    受け取ったmovesで裏返せる石の数が最小の手で相手に次のターンで相手にすべての石をひっくり返されない手を返す
+    """
+    sortedMoves = sort_moves_by_flippable(board, moves, player)  # ソート
+    for move in sortedMoves:
+        if check_my_stone_all_reverse(board, move) is True:  # ここでチェック関数を呼ぶ
+            return move
 
 
 def check_active_mode():
@@ -26,6 +39,30 @@ def check_active_mode():
     turnCnt += 2
 
     # print(f"turnCnt: {turnCnt} isActiveMode: {isActiveMode}")
+
+
+def sort_moves_by_flippable(board, moves, player):
+    """
+    ひっくり返せる石が少ない順にソートする
+    """
+    return sorted(
+        moves, key=lambda move: OthelloLogic.countFlippable(board, move, player, 8)
+    )
+
+
+def check_my_stone_all_reverse(board, move):
+    """
+    次のターンで相手にすべての石をひっくり返されないかチェックする
+    """
+    nextBoard = OthelloLogic.execute(copy.deepcopy(board), move, 1, 8)
+    nextEnemyMoves = OthelloLogic.getMoves(nextBoard, -1, 8)
+
+    for enemyMove in nextEnemyMoves:
+        nextNextBoard = OthelloLogic.execute(copy.deepcopy(nextBoard), enemyMove, -1, 8)
+        nextNextMyMoves = OthelloLogic.getMoves(nextNextBoard, 1, 8)
+        if len(nextNextMyMoves) == 0:
+            return False
+    return True
 
 
 def select_max_eval_moves(board, moves, player) -> List[int]:
